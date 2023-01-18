300d9d9eb2d9c87e2c1985f8d9310db1ea6cc0d2
Umair <tipusaeed501@gmail.com>
2023-01-18 20:11:24 +0500

    added modal for todo

--- src/app/components/TodoInput.tsx
+++ src/app/components/TodoInput.tsx
@@ -6,8 +6,9 @@ interface TodoProps {
 
 function TodoInput(props: TodoProps) {
   const { label, placeHolder, callBack } = props
+
   return (
-    <div className="flex">
+    <div className="flex w-80 mb-2 lg:mb-0">
       <input
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm w-56 rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder={placeHolder ?? label}
--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -1,17 +1,30 @@
+"use client"
 import TodoInput from "./TodoInput"
 import TodoList from "./TodoList"
+import AddTodoModal from "./AddTodoModal"
+import { useState } from "react"
 function Banner() {
+  const [open, setopen] = useState(false)
+  function handleTodoModal() {
+    setopen(!open)
+  }
+
   return (
     <div className="h-[calc(100vh-64px)]">
       <div className="p-20 h-full">
-        <div className="flex justify-evenly items-center m-5">
-          <TodoInput label="Add" placeHolder="Add Todo" />
+        <div className="flex justify-evenly items-center m-5 flex-col lg:flex-row">
+          <TodoInput
+            label="Add"
+            placeHolder="Add Todo"
+            callBack={handleTodoModal}
+          />
           <TodoInput label="Search" placeHolder="Search Todo" />
         </div>
         <div className="flex justify-center p-3 m-5">
           <TodoList />
         </div>
       </div>
+      <AddTodoModal open={open} close={handleTodoModal} />
     </div>
   )
 }
